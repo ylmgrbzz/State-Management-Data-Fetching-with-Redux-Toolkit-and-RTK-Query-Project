a9754c66955ce17d774e1d638973aabda21856ec
ylmgrbzz <yalim.gurbuz@std.yildiz.edu.tr>
2024-12-22 21:03:17 +0300

    Integrate productsApi into Redux store with middleware support

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "./features/counter/counterSlice";
+import productsApi from "./service/dummyData";
 
 export default configureStore({
   reducer: {
     counter: counterReducer,
+    [productsApi.reducerPath]: productsApi.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(productsApi.middleware),
 });
