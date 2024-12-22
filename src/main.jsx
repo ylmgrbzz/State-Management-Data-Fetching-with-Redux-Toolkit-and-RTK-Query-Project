5cbb4040e22377cfc9de571c45649ee9672b67f3
ylmgrbzz <yalim.gurbuz@std.yildiz.edu.tr>
2024-12-22 20:32:01 +0300

    Add Redux Toolkit and React-Redux integration with store configuration

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,10 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
+import { Provider } from "react-redux";
+import store from "./app/store.js";
 
-createRoot(document.getElementById("root")).render(<App />);
+createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
