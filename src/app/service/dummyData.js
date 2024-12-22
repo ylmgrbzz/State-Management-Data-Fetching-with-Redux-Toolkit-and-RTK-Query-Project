d9baef15b612b6244193cf9441351134c1ea08ed
ylmgrbzz <yalim.gurbuz@std.yildiz.edu.tr>
2024-12-22 20:51:03 +0300

    Add dummyApi service with endpoints for fetching products

---
+++ src/app/service/dummyData.js
@@ -0,0 +1,36 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const dummyApi = createApi({
+  reducerPath: "products",
+  baseQuery: fetchBaseQuery({ baseUrl: "https://dummyjson.com" }),
+  endpoints: (builder) => ({
+    getProducts: builder.query({
+      query: () => "products",
+    }),
+    getProduct: builder.query({
+      query: (id) => `products/${id}`,
+    }),
+    addProduct: builder.mutation({
+      query: (product) => ({
+        url: "products",
+        method: "POST",
+        body: product,
+      }),
+    }),
+    updateProduct: builder.mutation({
+      query: ({ id, ...product }) => ({
+        url: `products/${id}`,
+        method: "PUT",
+        body: product,
+      }),
+    }),
+    deleteProduct: builder.mutation({
+      query: (id) => ({
+        url: `products/${id}`,
+        method: "DELETE",
+      }),
+    }),
+  }),
+});
+
+export const { useGetProductsQuery } = dummyApi;
